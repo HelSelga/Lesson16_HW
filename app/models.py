21c94be119ec4b2c2187d97f5e23e3c32533a393
Stasy23 <anastasia.marochkina@kvint.io>
2022-03-18 20:54:38 +0300

    change models.py

--- app/models.py
+++ app/models.py
@@ -4,48 +4,46 @@ from sqlalchemy.orm import relationship
 
 class UserRole(db.Model):
     __tablename__ = 'user_roles'
-    id = db.Column(db.INTEGER, primary_key=True)
-    name = db.Column(db.String(200))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.INTEGER, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(200), nullable=False)
     last_name = db.Column(db.String(200), nullable=False)
-    age = db.Column(db.INTEGER)
+    age = db.Column(db.Integer)
     email = db.Column(db.String(200), unique=True)
-    role_id = db.Column(db.String(200), db.ForeignKey('user_roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('user_roles.id'))
     phone = db.Column(db.String(20), unique=True)
 
-    order = relationship('Order')
-    offer = relationship('Offer')
-    role = relationship('UserRole')
+    role = relationship('UserRole', foreign_keys=role_id)
 
 
 class Offer(db.Model):
     __tablename__ = 'offers'
-    id = db.Column(db.INTEGER, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
-    order_id = db.Column(db.INTEGER, db.ForeignKey('orders.id'))
-    executor_id = db.Column(db.INTEGER, db.ForeignKey('users.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    order = relationship('Order')
-    executor = relationship('User')
+    order = relationship('Order', foreign_keys=order_id)
+    executor = relationship('User', foreign_keys=executor_id)
 
 
 class Order(db.Model):
     __tablename__ = 'orders'
-    id = db.Column(db.INTEGER, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     description = db.Column(db.String)
     start_date = db.Column(db.String)
     end_date = db.Column(db.String)
     address = db.Column(db.String)
-    price = db.Column(db.INTEGER)
+    price = db.Column(db.Integer)
 
-    customer_id = db.Column(db.INTEGER, db.ForeignKey('users.id'))
-    executor_id = db.Column(db.INTEGER, db.ForeignKey('users.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    customer = relationship('User', foreign_keys='Order.customer_id')
-    executor = relationship('User', foreign_keys='Order.executor_id')
+    customer = relationship('User', foreign_keys=customer_id)
+    executor = relationship('User', foreign_keys=executor_id)
